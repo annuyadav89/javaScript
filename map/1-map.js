2fec78b7d2200e10d96110ec2c9c7513e8c9187c
annuyadav89 <123576450+annuyadav89@users.noreply.github.com>
2023-09-12 19:07:33 +0000

    container config done

---
+++ map/1-map.js
@@ -0,0 +1,12 @@
+// var temp=[10,20,30,40,50];
+//     var finalResult =temp.map(function(obj){
+//                return obj*5;
+//     });
+//     console.log("original array",temp);
+//     console.log("new array",finalResult);
+    var arr = [10,30,50,500];
+    var result = arr.map(function(obj){
+        return obj/5;
+    });
+    console.log("original",arr);
+    console.log("new" , result);
